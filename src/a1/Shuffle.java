0fa5a0cb8dbcc3e42c6bb7c8afd9712f4d7182cc
Quinn Rohlf <qr@qrohlf.com>
2013-09-12 08:30:59 -0700

    Shuffle test code is there, but I'm not sure if badShuffle is implemented correctly

--- src/a1/Shuffle.java
+++ src/a1/Shuffle.java
@@ -15,15 +15,64 @@ public class Shuffle {
 			a[r] = temp;
 		}
 	}
+	
+	public static void badShuffle(double[] a) {
+		int N = a.length;
+		for (int i = 0; i < N; i++) {
+			int r = uniform(N - i);
+			double temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
 
-	public static void testShuffle(int i, int j) {
-		// TODO Auto-generated method stub
+	public static void testShuffle(int m, int n) {
+		double[] a = new double[m];
+		int[][] results = new int[m][m];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = i;
+		}
+		for (int i = 0; i < n; i++) {
+			shuffle(a);
+			for (int j = 0; j < a.length; j++) {
+				results[(int)a[j]][j]++;
+			}
+		}
+		printResults(results);
+	}
+
+	private static void printResults(int[][] results) {
+		int m = results.length;
+		println("            # of times in position");
+		println();
+		printf("           ");
+		for (int i = 0; i < m; i++) {printf("%-7d", i);}
+		printf("\n         ");
+		for (int i = 0; i < m*7; i++) {printf("-");}
+		println();
+		for (int i = 0; i < results.length; i++) {
+			printf("%s%6d| ", (i==m/2)?"# ":"  ", i);
+			for (int j = 0; j < results[i].length; j++) {
+				printf("%-6d ", results[i][j]);
+			}
+			println();
+		}
 		
 	}
 
-	public static void testBadShuffle(int i, int j) {
-		// TODO Auto-generated method stub
-		
+	public static void testBadShuffle(int m, int n) {
+		double[] a = new double[m];
+		int[][] results = new int[m][m];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = i;
+		}
+		for (int i = 0; i < n; i++) {
+			badShuffle(a);
+			for (int j = 0; j < a.length; j++) {
+				results[(int)a[j]][j]++;
+			}
+		}
+		printResults(results);
 	}
 
 }
