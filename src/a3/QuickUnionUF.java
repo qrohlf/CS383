e2eaacb1a77a7837580709c0976c5961171ce5f5
Ben Whitehead <benw5858@gmail.com>
2013-09-30 11:27:57 -0700

    a3 Bones
    Basic setup for a3. all methods have been generated by eclipse so buyer
    beware!

---
+++ src/a3/QuickUnionUF.java
@@ -0,0 +1,22 @@
+package a3;
+
+import edu.princeton.cs.algs4.QuickUnionUF;
+
+
+public class QuickUnionUF {
+
+	public QuickUnionUF(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int[] getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void union(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/a3/TwoStackQueue.java
@@ -0,0 +1,20 @@
+package a3;
+
+public class TwoStackQueue<S> {
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void enqueue(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
